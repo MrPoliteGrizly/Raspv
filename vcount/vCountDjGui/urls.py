bae2d1660e77493432cb178d85ed758306a9d66e
root <root@localhost.localdomain>
2017-01-05 13:05:49 +0600

    New step to admin

--- vcount/vCountDjGui/urls.py
+++ vcount/vCountDjGui/urls.py
@@ -17,7 +17,7 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
+    (r'^admin/', include(admin.site.urls)),
     (r'^$', include('settings.urls')),
     (r'^settings/', include('settings.urls')),
     (r'^channels/$', include('channels.urls')),
